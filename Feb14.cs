5fe43ad11504a80e909386f10fee0fd36dae74c4
RohRoh556 <RohinT369@stemcollegiate.ca>
2025-02-14 12:02:14 -0500

    Create Feb14.cs

---
+++ Feb14.cs
@@ -0,0 +1,7 @@
+//This is my code for Febuary 14th 
+//We learned how to store varibles 
+string firstName = Rohin;
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
