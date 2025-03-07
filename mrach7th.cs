ae8ae67a594bf75d28dbde1228f1fad531dec07d
RohRoh556 <RohinT369@stemcollegiate.ca>
2025-03-07 12:14:55 -0500

    Create mrach7th.cs

---
+++ mrach7th.cs
@@ -0,0 +1,17 @@
+// This is my code for march 7th 
+// I learned how to string values 
+string firstName; 
+firstName = "Rohin";
+Console.WriteLine("Rohin");
+var message = "Fortnite!";
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
