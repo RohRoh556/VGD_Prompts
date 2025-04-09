8dff411517a7f50d35afc2951080f94e0f5fb7fe
RohRoh556 <RohinT369@stemcollegiate.ca>
2025-04-09 10:47:26 -0400

    Create april8.cs

---
+++ april8.cs
@@ -0,0 +1,17 @@
+//This is my codes for april 8th
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
+
+decimal decimalQuotient = 7.0m / 5;
+Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+
+decimal decimalQuotient = 7 / 5.0m;
+decimal decimalQuotient = 7.0m / 5.0m;
+//I learned how to increase the value of the varibles
