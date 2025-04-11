dff7ddfe4ed8d08470e457e0242f64ae340eaef9
RohRoh556 <RohinT369@stemcollegiate.ca>
2025-04-11 12:16:49 -0400

    Create april9th.cs

---
+++ april9th.cs
@@ -0,0 +1,7 @@
+// these are my codes for april 9th 
+int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32) * (5m / 9m);
+Console.WriteLine($"the temperature is {celsius} Celsius.");
+output
+the temperature is 34.444444444444444444444444447 Celsius.
+  // I learned how to convert celsius to fahrenheit
